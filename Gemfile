eefdf6db40aa8f34c8c5d1fcabb2af806cb00b49
worace <horace.d.williams@gmail.com>
2015-07-15 17:48:52 -0600

    install and configure omniauth

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'omniauth-twitter'
 
 group :development, :test do
   gem 'byebug'
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
+
 module OauthWorkshop
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
