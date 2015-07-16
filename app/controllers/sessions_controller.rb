90e6d9e81610087f2e763b4e0e013118dcc5c78a
worace <horace.d.williams@gmail.com>
2015-07-15 20:43:48 -0600

    step 11, logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,9 @@ class SessionsController < ApplicationController
     end
     redirect_to root_path
   end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "welcome#index"
   get "/auth/twitter", as: :login
   get "/auth/twitter/callback" => "sessions#create"
+  get "/logout" => "sessions#destroy", as: :logout
 end
