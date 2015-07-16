a83774fb49bd416eb8eb5f38b62c44584446a5ab
worace <horace.d.williams@gmail.com>
2015-07-15 20:14:02 -0600

    step 9: creating users from oauth data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(auth_info)
+    if user = find_by(uid: auth_info.extra.raw_info.user_id)
+      user
+    else
+      create({name: auth_info.extra.raw_info.name,
+              screen_name: auth_info.extra.raw_info.screen_name,
+              uid: auth_info.extra.raw_info.user_id,
+              oauth_token: auth_info.credentials.token,
+              oauth_token_secret: auth_info.credentials.secret})
+    end
+  end
 end
